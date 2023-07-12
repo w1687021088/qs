ce3a8553135a2b150d268fa27555bae41505cb2c
zhangjiewu <zhangjw@kh-super.com>
2023-07-12 16:50:26 +0800

    dosc： 数组合并

---
+++ javaScript/手写代码/合并数组.js
@@ -0,0 +1,21 @@
+/**
+ *
+ * 输入为两个一维数组，将这两个数组合并，去重，不要求排序，返回一维数组
+ *
+ * */
+function deal(...args) {
+  return Array.from(new Set([...args].flat()))
+}
+
+console.log(deal([1, 2, 3], [0, 3, 4, 5], [5, 6, 7, 8], [4, 5, 6]))
+
+/**
+ *
+ * 要求排序
+ *
+ * */
+function deal2(...args) {
+  return Array.from(new Set([...args].flat())).sort((a, b) => a - b)
+}
+
+console.log(deal2([1, 2, 3], [0, 3, 4, 5], [5, 6, 7, 8], [4, 5, 6]))
