44bbd522961ab82a34fa9fc57349d2c1e34c7989
zhangjiewu <zhangjw@kh-super.com>
2023-07-12 11:47:31 +0800

    dosc： 手写 new 补充注释描述

--- javaScript/手写代码/手写new.js
+++ javaScript/手写代码/手写new.js
@@ -25,18 +25,33 @@ const foo = new Foo('张张')
  *
  * */
 
-function _new(_function, ...ages) {
-  if (typeof _function !== 'function') {
+function _new(constructor, ...ages) {
+  if (typeof constructor !== 'function') {
     throw '_function 不是一个构造函数'
   }
 
-  const $object = Object.create(_function.prototype)
+  // 创建一个空对象，将构造函数的prototype设置为新对象的原型。
 
+  // Object.create() 静态方法以一个现有对象作为原型，创建一个新对象
+  const $object = Object.create(constructor.prototype) // 推荐这个方式
+
+  // or 也可以采用已下的方式
+  // const $object = {}
+  // $object.__proto__ = constructor.prototype // Object.setPrototypeOf($object, constructor.prototype)
+  /**
+   *
+   * 不推荐这种方式 $object.__proto__
+   *
+   * Object.setPrototypeOf($object, constructor.prototype) 不是很推荐，尽量使用 Object.create() 创建一个对象，并将原型指向它。
+   *
+   * */
+
+  console.log($object)
   // 执行构造函数，并将this上下文指向该构造函数
-  const value = _function.apply($object, ages)
+  const value = constructor.apply($object, ages)
 
   // 如果函数有返回类型是对象时，在构造函数中如果返回一个对象类型，则将覆盖原有在构造函数定义的属性。
-  return value instanceof Object ? value : $object
+  return typeof value === 'object' || typeof value === 'function' ? value : $object
 }
 
 const foe = _new(Foo, '李李')
